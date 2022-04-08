6fd25b49bba0bfaa8d036e7b89b7f4231783c304
Jeremy <jplatt4427@gmail.com>
2022-04-08 10:24:22 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function findWin(record){
+    if (record.result === 'W'){
+        
+        return record.year;
+    } else{
+        return undefined;
+    }
+}
+
+function superbowlWin(record){
+    let findYear = record.find(findWin);
+    if(findYear != undefined){
+     let foundYear = findYear.year;
+     console.log(foundYear);
+     return foundYear;
+    } else {
+        return undefined;
+    }
+
+}
+
